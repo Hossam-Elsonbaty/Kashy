addd285b807ec269f5ed4544293c854e19dcd0a0
Mostafa Elsonbaty <m.sonbaty.contact@gmail.com>
2026-01-08 21:28:11 +0200

    Update ConfirmDelete.tsx

--- src/components/ConfirmDelete.tsx
+++ src/components/ConfirmDelete.tsx
@@ -4,6 +4,7 @@ const ConfirmDelete = ({isOpen, handleOpen, handleDelete}:{isOpen: boolean; hand
       <div className="relative p-4 w-full max-w-md max-h-full">
         <div className="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
           <button
+            onClick = {handleOpen}
             type="button"
             className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
             data-modal-hide="popup-modal"
